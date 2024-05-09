d35a21ef44c59b57fad726b4ed8eb963d03eb8b2
gurbanli11 <orkhan.80@bk.ru>
2024-05-10 00:59:51 +0400

    admin panel

---
+++ MultiShop/Program.cs
@@ -0,0 +1,23 @@
+namespace MultiShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            //app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.MapControllerRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+            app.MapControllerRoute(
+                "default",
+                "{controller=Home}/{action=Index}"
+                );
+
+            app.Run();
+        }
+    }
+}
